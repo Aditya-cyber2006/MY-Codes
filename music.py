c1ba1a048a2d3da0f55f3dfcc5421f6d9ded87ea
Aditya-cyber2006 <amungase20@gmail.com>
2025-08-31 21:44:52 +0530

    Add files via upload

---
+++ music.py
@@ -0,0 +1,8 @@
+music_library={"candy":"https://www.youtube.com/watch?v=AYZIIATwgks",
+"montero":"https://www.youtube.com/watch?v=nsXwi67WgOo",
+"worth":"https://www.youtube.com/watch?v=Cw1qnwbIEFY",
+"rasputin":"https://www.youtube.com/watch?v=x5Oag4hISgU",
+"shake it to the max":"https://www.youtube.com/watch?v=OBvzltuwk3c"
+               
+               
+               }
